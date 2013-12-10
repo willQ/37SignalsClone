abd5341bfdb45fdb3c88f00407b433a45c76209b
willQ <will.mcquain@gmail.com>
2013-12-10 09:33:11 -0500

    Functioning First Draft
    Many many changes: functioning arrows, positioning, changing style of
    hidden containers, h1s and p tags.

--- application.js
+++ application.js
@@ -2,27 +2,34 @@ $('document').ready(function() {
 	$('.basecamp_btn').mouseenter(function () {
 		$('.mainlogo_text').hide();
 		$('.basecamp_text').show();
+		$('div.basecamp_btn').append("<img class='arrow' src='images/arrow-left.png'>");
 	});
 	$('.basecamp_btn').mouseleave(function () {
 		$('.basecamp_text').hide();
 		$('.mainlogo_text').show();
+		$('img.arrow').remove();
+
 	});
 
 	$('.highrise_btn').mouseenter(function () {
 		$('.mainlogo_text').hide();
 		$('.highrise_text').show();
+		$('div.highrise_btn').append("<img class='arrow' src='images/arrow-left.png'>");
 	});
 	$('.highrise_btn').mouseleave(function () {
 		$('.highrise_text').hide();
 		$('.mainlogo_text').show();
+		$('img.arrow').remove();
 	});
 
 	$('.campfire_btn').mouseenter(function () {
 		$('.mainlogo_text').hide();
 		$('.campfire_text').show();
+		$('div.campfire_btn').append("<img class='arrow_right' src='images/arrow-right.png'>");
 	});
 	$('.campfire_btn').mouseleave(function () {
 		$('.campfire_text').hide();
 		$('.mainlogo_text').show();
+		$('img.arrow_right').remove();
 	});
 });
